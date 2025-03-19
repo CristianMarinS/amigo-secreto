5f6b0e9b91ac8d5e2bdff3029ab751a4d87b05df
CristianMarinSuarez <cristianms45144@gmail.com>
2025-03-18 19:09:14 -0500

    Agregado README.md y mejoras en la lógica

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # amigo-secreto
+
+//esta aplicacion es una página que nos permite sortear amigos secretos, tiene validaciones y funciona bien en general.
+utilizando css html y js
+Cristian Marín - GitHub
+Este proyecto está bajo la licencia MIT.
--- app.js
+++ app.js
@@ -1,1 +1,45 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+
+function nuevoAmigo() {
+  const input = document.getElementById("amigo");
+  const nombre = input.value.trim();
+
+  if (nombre === "") {
+    alert("Por favor, ingrese un nombre que sea válido.");
+    return;
+  }
+
+  if (listaAmigos.includes(nombre)) {
+    alert(
+      "Ese nombre ya está en la lista, ingresa uno diferente o ponle un distintivo!! ."
+    );
+    return;
+  }
+
+  listaAmigos.push(nombre);
+  actualizar();
+  input.value = "";
+}
+function actualizar() {
+  const ul = document.getElementById("listaAmigos");
+  ul.innerHTML = "";
+
+  listaAmigos.forEach((nombre) => {
+    const li = document.createElement("li");
+    li.textContent = nombre;
+    ul.appendChild(li);
+  });
+}
+
+function sorteo() {
+  if (listaAmigos.length === 0) {
+    alert("Agrega al menos un nombre para el sorteo.");
+    return;
+  }
+
+  const indiceAleatorio = Math.floor(Math.random() * listaAmigos.length);
+  const amigoSecreto = listaAmigos[indiceAleatorio];
+
+  const resultado = document.getElementById("resultado");
+  resultado.innerHTML = `<li> ${amigoSecreto} es el amigo secreto! !!! <3</li>`;
+}
